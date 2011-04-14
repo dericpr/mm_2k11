1bd4a914131f80fe42d786b1e905c9aa6193711c
Deric Panet-Raymond <dericpr@gmail.com>
2011-04-13 23:48:10 -0400

    Testing Git tie-in

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <script>
          $(document).ready(function(){
            $("a").click(function(event){
-             alert("As you can see, the link no longer took you to jquery.com");
+             alert("Is Git working?");
              event.preventDefault();
            });
          });
