bd28d5982eba291ad83b8af1fff55eec1b7d0b34
Deric Panet-Raymond <dericpr@gmail.com>
2011-04-13 23:43:49 -0400

    Initial Jquery tie-in

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Murder Mystery Party - 2011</title>
+         <script src="jquery.js"></script>
+    </head>
+    <body>
+        <a href="http://jquery.com/">jQuery</a>
+        <script>
+         $(document).ready(function(){
+           $("a").click(function(event){
+             alert("As you can see, the link no longer took you to jquery.com");
+             event.preventDefault();
+           });
+         });
+        </script>
+        <?php
+        print("HEYLOOOOO");
+        ?>
+    </body>
+</html>
